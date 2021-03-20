467b653af52998e637b9a4b3fa72b5aef1240db9
Ydjin <akk-06@mail.ru>
2021-03-20 16:28:05 +0700

    implemented spline construction

---
+++ main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    x = [-20, -3, -4, 0, 1, 5, 6, 7, 20]
+    x.sort()
+
+    y = [value ** 3 for value in x]
+
+    x_triplets = build_triplets(x)
+    y_triplets = build_triplets(y)
+
+    x_general = []
+    lagr = []
+
+    for i in range(len(x_triplets)):
+        x_new = np.linspace(x_triplets[i][0], x_triplets[i][-1], 50)
+        x_general.extend(x_new)
+        for j in x_new:
+
+            lagr.append(lagrange(y_triplets[i], x_triplets[i], j))
+
+    plt.plot(x, y, 'o', x_general, lagr)
+    plt.show()
+
+
+def build_triplets(x: list):
+    triplets = []
+    for i in range(0, len(x), 2):
+        if i == 0:
+            continue
+
+        triplets.append([x[i - 2], x[i - 1], x[i]])
+
+    if x[-1] not in triplets[-1]:
+        triplets.append([triplets[-1][2], x[-1]])
+
+    return triplets
+
+
+def lagrange(y_triplet, x_triplet, x):
+    return y_triplet[0] * ((x - x_triplet[1]) * (x - x_triplet[2])) / (
+                (x_triplet[0] - x_triplet[1]) * (x_triplet[0] - x_triplet[2])) \
+           + y_triplet[1] * ((x - x_triplet[0]) * (x - x_triplet[2])) / (
+                       (x_triplet[1] - x_triplet[0]) * (x_triplet[1] - x_triplet[2])) \
+           + y_triplet[2] * ((x - x_triplet[0]) * (x - x_triplet[1])) / (
+                       (x_triplet[2] - x_triplet[0]) * (x_triplet[2] - x_triplet[1]))
+
+
+if __name__ == '__main__':
+    main()
